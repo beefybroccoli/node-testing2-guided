98731023558816d8efd3f60bb943808022c15de9
fred <tedkimemail@yahoo.com>
2021-11-11 17:03:01 -0800

    ...

--- api/hobbits/hobbits-model.test.js
+++ api/hobbits/hobbits-model.test.js
@@ -2,18 +2,20 @@ const { testing } = require("../../knexfile");
 const modelHobit = require("./hobbits-model");
 const db = require("../../data/dbConfig");
 
+//sanity test
 test('is testing environment', ()=>{
     expect(process.env.NODE_ENV).toBe('testing');
 })
 
 beforeAll(async()=>{
-
+    await db.migrate.rollback();
+    await db.migrate.latest();
 })
 
 beforeEach(async()=>{
-    
+    await db.seed.run(); //seeding the database
 })
 
 afterAll(async()=>{
-    
+    await db.destroy(); //disconnect from a database
 })
